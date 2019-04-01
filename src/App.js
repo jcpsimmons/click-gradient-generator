bd401a696687b692d9df0af6345f88c29f3cfdcb
simsies <jcpsimmons@gmail.com>
2019-04-01 10:25:01 -0700

    working! just need to find out how to export...

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 var randomColor = require('randomcolor');
 
-
+let randomDegrees = Math.random() * 360;
+let randomStartColor = randomColor();
+let randomEndColor = randomColor();
 
 class App extends Component {
 
   constructor(props) {
    super(props);
    this.state = {
-     bgImg: 'linear-gradient(30deg, red, yellow)'
+     bgImg: 'linear-gradient(' + randomDegrees + 'deg' + ', ' + randomStartColor + ', ' + randomEndColor + ')'
    }
  }
 
+
  clickDiv = (e) => {
-   let randomDegrees = Math.random() * 360;
+   randomDegrees = Math.random() * 360;
+   randomStartColor = randomColor();
+   randomEndColor = randomColor();
     this.setState({
-      bgImg: 'linear-gradient(' + randomDegrees + 'deg' + ', ' + randomColor() + ', ' + randomColor() + ')'
+      bgImg: 'linear-gradient(' + randomDegrees + 'deg' + ', ' + randomStartColor + ', ' + randomEndColor + ')'
     })
   }
 
@@ -25,7 +30,25 @@ class App extends Component {
     return (
       <div className="Full-div"
         style={{backgroundImage: this.state.bgImg}}
-             onClick={this.clickDiv}>red</div>
+             onClick={this.clickDiv}>
+             <div className="Center-div">
+               <h1>
+                 random gradient generator
+               </h1>
+               <h2>
+                 tap anywhere
+               </h2>
+               <h2>
+                 {String(randomStartColor)} -> {String(randomEndColor)}
+               </h2>
+               <h2>
+                 direction: {String(Math.floor(randomDegrees))}&deg;
+               </h2>
+               <p>
+                 made in React by <a href="https://jcpsimmons.github.io/">Josh C. Simmons</a>
+               </p>
+             </div>
+           </div>
     );
   }
 }
